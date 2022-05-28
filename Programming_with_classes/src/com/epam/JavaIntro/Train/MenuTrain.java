94823577969e5744e19be761c1650ed6236e6bc2
KorolevaOlga <84726342+KorolevaOlga@users.noreply.github.com>
2022-05-28 10:52:25 +0300

    Add files via upload

---
+++ Programming_with_classes/src/com/epam/JavaIntro/Train/MenuTrain.java
@@ -0,0 +1,58 @@
+package Program.Train1;
+
+import java.util.Scanner;
+
+public class MenuTrain {
+    private final Scanner scan;
+
+    public MenuTrain(Scanner scan) {
+        this.scan = scan;
+    }
+
+    private void inMenu() {
+
+        System.out.println("Сведения о поездах: " +
+                "\n 1 - Для получения информации по номеру поезда;" +
+                        "\n 2 - Для сортировки информации о поездах по пункту назначения " +
+                        "\n (Поезда с одинаковыми пунктами назначения упорядочены по времени отправления);" +
+                        "\n 3 - Для сортировки информации по номерам поездов;" +
+                        "\n 4 - Для выхода из меню"
+        );
+    }
+
+    public void start(Train[] trains) {
+        if (this.scan != null) {
+            int key;
+            do {
+                inMenu();
+                System.out.println("Введите цифры согласно запросу: ");
+                key = this.scan.nextInt();
+
+                switch (key) {
+                    case 1:
+                        Scanner scanner = new Scanner(System.in);
+                        System.out.println("Введите номер поезда: ");
+                        int number = scanner.nextInt();
+                        Train.trainInfo(number, trains);
+                        System.out.println();
+                        break;
+                    case 2:
+                        Train.trainDestination(trains);
+                        Train.printTrains(trains);
+                        System.out.println();
+                        break;
+                    case 3:
+                        Train.sorting(trains);
+                        Train.printTrains(trains);
+                        System.out.println();
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        System.out.println("Вы ввели неверное значение для меню \n");
+                }
+
+            }while (key != 4);
+        }
+    }
+}
---
+++ Programming_with_classes/src/com/epam/JavaIntro/Train/Train.java
@@ -0,0 +1,124 @@
+package Program.Train1;
+
+
+import java.util.Objects;
+
+public class Train {
+    private String destination;
+    private int trainNumber;
+    private String departureTime;
+
+    public Train() {
+    }
+
+    public Train(String destination, int trainNumber, String departureTime) {
+        this.destination = destination;
+        this.trainNumber = trainNumber;
+        this.departureTime = departureTime;
+    }
+    public static void trainInfo(int number, Train[] trains) {
+        for (Train train : trains) {
+            if (train.trainNumber == number) {
+                System.out.println("Номер поезда: " + train.trainNumber + "\n Пункт назначения: " + train.destination + "\nВремя отправления: " + train.departureTime);
+            }
+        }
+
+    }
+
+    public static void sorting(Train[] trains) {
+        Train temp;
+        boolean flag = true;
+        while (flag) {
+            flag = false;
+            for (int i = 0; i < trains.length - 1; i++) {
+                if (trains[i].trainNumber > trains[i + 1].trainNumber) {
+                    temp = trains[i];
+                    trains[i] = trains[i + 1];
+                    trains[i + 1] = temp;
+                    flag = true;
+                }
+            }
+
+        }
+    }
+    public static void printTrains(Train[] trains) {
+        for (Train train : trains) {
+            System.out.println("Номер поезда: " + train.trainNumber + "\n Пункт назначения: " + train.destination + "\nВремя отправления: " + train.departureTime);
+        }
+    }
+
+    public static void trainDestination(Train[] trains) {
+        Train temp;
+        boolean flag = true;
+
+        while (flag) {
+            flag = false;
+
+            for (int i = 0; i < trains.length - 1; i++) {
+                int comp = trains[i].destination.compareTo(trains[i + 1].destination);
+                if (comp > 0) {
+                    temp = trains[i];
+                    trains[i] = trains[i + 1];
+                    trains[i + 1] = temp;
+                    flag = true;
+                } else if (comp == 0) {
+                    if (trains[i].departureTime.compareTo(trains[i + 1].departureTime) > 0) {
+                        temp = trains[i];
+                        trains[i] = trains[i + 1];
+                        trains[i + 1] = temp;
+                        flag = true;
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getTrainNumber() {
+        return trainNumber;
+    }
+
+    public void setTrainNumber(int trainNumber) {
+        this.trainNumber = trainNumber;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Train)) return false;
+        Train train = (Train) o;
+        return getTrainNumber() == train.getTrainNumber() && Objects.equals(getDestination(), train.getDestination()) && Objects.equals(getDepartureTime(), train.getDepartureTime());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getDestination(), getTrainNumber(), getDepartureTime());
+    }
+
+    @Override
+    public String toString() {
+        return "Train{" +
+                "destination='" + destination + '\'' +
+                ", trainNumber=" + trainNumber +
+                ", departureTime='" + departureTime + '\'' +
+                '}';
+    }
+}
+
