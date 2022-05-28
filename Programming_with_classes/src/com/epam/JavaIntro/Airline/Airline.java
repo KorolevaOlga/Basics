81afb9e0edc1e5fe6cc8acd4c8b8bb2c0f13e9f4
KorolevaOlga <84726342+KorolevaOlga@users.noreply.github.com>
2022-05-28 14:02:42 +0300

    Add files via upload

---
+++ Programming_with_classes/src/com/epam/JavaIntro/Airline/Airline.java
@@ -0,0 +1,92 @@
+package Program.Airline;
+
+import java.util.Objects;
+
+public class Airline {
+    {
+        startId++;
+    }
+    private static int startId = 0;
+    private int id = startId;
+
+private String destination;
+private String flightNumber;
+private String aircraftType;
+private double departureTime;
+private String dayWeek;
+
+    public Airline() {
+    }
+
+    public Airline(String destination, String flightNumber, String aircraftType, double departureTime, String dayWeek) {
+    this.destination = destination;
+    this.flightNumber = flightNumber;
+    this.aircraftType = aircraftType;
+    this.departureTime = departureTime;
+    this.dayWeek = dayWeek;
+}
+
+    public String toSting(){
+        String string = "";
+        string = string + "Id: " + id + "\n Пункт назначения: " + destination + "\n Номер рейса: " + flightNumber +
+                "\n Тип самолета: " + aircraftType + "\n Время вылета: " + departureTime + "\n День недели: " + dayWeek;
+        return string;
+    }
+
+    public void print(){
+        System.out.println(toSting());
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public void setAircraftType(String aircraftType) {
+        this.aircraftType = aircraftType;
+    }
+
+    public void setDepartureTime(int departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public void setDayWeek(String dayWeek) {
+        this.dayWeek = dayWeek;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public String getAircraftType() {
+        return aircraftType;
+    }
+
+    public double getDepartureTime() {
+        return departureTime;
+    }
+
+    public String getDayWeek() {
+        return dayWeek;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Airline)) return false;
+        Airline airline = (Airline) o;
+        return id == airline.id && Double.compare(airline.getDepartureTime(), getDepartureTime()) == 0 && Objects.equals(getDestination(), airline.getDestination()) && Objects.equals(getFlightNumber(), airline.getFlightNumber()) && Objects.equals(getAircraftType(), airline.getAircraftType()) && Objects.equals(getDayWeek(), airline.getDayWeek());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, getDestination(), getFlightNumber(), getAircraftType(), getDepartureTime(), getDayWeek());
+    }
+}
---
+++ Programming_with_classes/src/com/epam/JavaIntro/Airline/AirlineBase.java
@@ -0,0 +1,81 @@
+package Program.Airline;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class AirlineBase {
+
+    private ArrayList<Airline> base;
+
+public AirlineBase() {
+        this.base = new ArrayList<>();
+}
+
+    public AirlineBase(ArrayList<Airline> base) {
+        this.base = base;
+    }
+
+    public void addAirlineBase(Airline airline) {
+        base.add(airline);
+    }
+
+    List<Airline> destinations(String destination) {
+        List<Airline> destinations = new ArrayList<>();
+        for (Airline b: base) {
+            if (b.getDestination().equals(destination)) {
+                destinations.add(b);
+            }
+        }
+        return destinations;
+    }
+
+    List<Airline> days(String dayWeek) {
+        List<Airline> days = new ArrayList<>();
+        for (Airline b: base) {
+            if (b.getDayWeek().equals(dayWeek)) {
+                days.add(b);
+            }
+        }
+        return days;
+
+    }
+
+    List<Airline> daysAndTime(String dayWeek, double departureTime) {
+        List<Airline> days2 = new ArrayList<>();
+        for (Airline b: base) {
+            if (b.getDayWeek().equals(dayWeek) && b.getDepartureTime() > departureTime) {
+                    days2.add(b);
+                }
+            }
+        return days2;
+    }
+
+    public ArrayList<Airline> getBase() {
+        return base;
+    }
+
+    public void setBase(ArrayList<Airline> base) {
+        this.base = base;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AirlineBase)) return false;
+        AirlineBase that = (AirlineBase) o;
+        return Objects.equals(getBase(), that.getBase());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getBase());
+    }
+
+    @Override
+    public String toString() {
+        return "AirlineBase{" +
+                "base=" + base +
+                '}';
+    }
+}
