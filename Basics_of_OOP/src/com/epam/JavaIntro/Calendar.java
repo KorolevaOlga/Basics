8c687a96067a429825e35d697a5e108fb2683ecc
KorolevaOlga <84726342+KorolevaOlga@users.noreply.github.com>
2022-05-30 17:28:39 +0300

    Add files via upload

---
+++ Basics_of_OOP/src/com/epam/JavaIntro/Calendar.java
@@ -0,0 +1,170 @@
+package Calendar;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Calendar {
+
+    private List<Dates> dates;
+
+    public Calendar() {
+    }
+
+    public Calendar(List<Dates> dates) {
+        this.dates = dates;
+    }
+
+    public List<Dates> getDates() {
+        return dates;
+    }
+
+    public void setDates(List<Dates> dates) {
+        this.dates = dates;
+    }
+
+    public void infoDays(int number, Dates.Month month, int year) {
+        for (int i = 0; i < dates.size(); i++) {
+            if ((number == dates.get(i).number && dates.get(i).month.equals(month)) && dates.get(i).year == year) {
+                System.out.println(dates.get(i).type);
+            }
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Calendar)) return false;
+        Calendar calendar = (Calendar) o;
+        return Objects.equals(getDates(), calendar.getDates());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getDates());
+    }
+
+    @Override
+    public String toString() {
+        return "Calendar{" +
+                "dates=" + dates +
+                '}';
+    }
+
+    public static class Dates {
+
+        int number;
+        Month month;
+        int year;
+        DaysWeek daysWeek;
+        Type type;
+
+        public Dates() {
+        }
+
+        public Dates(int number, Month month, int year, DaysWeek daysWeek, Type type) {
+            this.number = number;
+            this.month = month;
+            this.year = year;
+            this.daysWeek = daysWeek;
+            this.type = type;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public void setNumber(int number) {
+            this.number = number;
+        }
+
+        public Month getMonth() {
+            return month;
+        }
+
+        public void setMonth(Month month) {
+            this.month = month;
+        }
+
+        public int getYear() {
+            return year;
+        }
+
+        public void setYear(int year) {
+            this.year = year;
+        }
+
+        public DaysWeek getDaysWeek() {
+            return daysWeek;
+        }
+
+        public void setDaysWeek(DaysWeek daysWeek) {
+            this.daysWeek = daysWeek;
+        }
+
+        public Type getType() {
+            return type;
+        }
+
+        public void setType(Type type) {
+            this.type = type;
+        }
+
+        enum Month {
+            JANUARY,
+            FEBRUARY,
+            MARCH,
+            APRIL,
+            MAY,
+            JUNE,
+            JULY,
+            AUGUST,
+            SEPTEMBER,
+            OCTOBER,
+            NOVEMBER,
+            DECEMBER
+        }
+
+        enum DaysWeek {
+            MONDAY,
+            TUESDAY,
+            WEDNESDAY,
+            THURSDAY,
+            FRIDAY,
+            SATURDAY,
+            SUNDAY
+        }
+
+        enum Type {
+            WORK_DAY,
+            CELEBRATION_WORK_DAY,
+            CELEBRATION_NOT_WORKING,
+            DAY_OFF,
+            CELEBRATION_DAY_OFF
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Dates dates = (Dates) o;
+            return number == dates.number && year == dates.year && month == dates.month && daysWeek == dates.daysWeek && type == dates.type;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(number, month, year, daysWeek, type);
+        }
+
+        @Override
+        public String toString() {
+            return "Dates{" +
+                    "number=" + number +
+                    ", month=" + month +
+                    ", year=" + year +
+                    ", daysWeek=" + daysWeek +
+                    ", type=" + type +
+                    '}';
+        }
+    }
+
+}
---
+++ Basics_of_OOP/src/com/epam/JavaIntro/Main.java
@@ -0,0 +1,28 @@
+package Calendar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calendar.Dates day = new Calendar.Dates(1, Calendar.Dates.Month.APRIL, 2022, Calendar.Dates.DaysWeek.FRIDAY, Calendar.Dates.Type.WORK_DAY);
+       Calendar.Dates day1 = new Calendar.Dates(2, Calendar.Dates.Month.APRIL, 2022, Calendar.Dates.DaysWeek.SATURDAY, Calendar.Dates.Type.DAY_OFF);
+       Calendar.Dates day2 = new Calendar.Dates(3, Calendar.Dates.Month.APRIL, 2022, Calendar.Dates.DaysWeek.SUNDAY, Calendar.Dates.Type.DAY_OFF);
+       Calendar.Dates day3 = new Calendar.Dates(4, Calendar.Dates.Month.APRIL, 2022, Calendar.Dates.DaysWeek.MONDAY, Calendar.Dates.Type.WORK_DAY);
+
+        List<Calendar.Dates> dates = new ArrayList<Calendar.Dates>();
+
+        dates.add(day);
+        dates.add(day1);
+        dates.add(day2);
+        dates.add(day3);
+
+        Calendar calendar = new Calendar(dates);
+
+        calendar.infoDays(1, Calendar.Dates.Month.APRIL, 2022);
+
+    }
+}
+
