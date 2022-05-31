c58cdfb4d490575d52327e433a9388f46a737b0b
KorolevaOlga <84726342+KorolevaOlga@users.noreply.github.com>
2022-05-31 17:32:02 +0300

    Add files via upload

--- Basics_of_OOP/src/com/epam/JavaIntro/Flowers/main/Main.java
+++ Basics_of_OOP/src/com/epam/JavaIntro/Flowers/main/Main.java
@@ -1,10 +1,10 @@
-package com.epam.JavaIntro.Flowers.main;
+package flovers.flowers.src.Program;
 
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-      new MenuBouquet(new Scanner(System.in)).start();
+        new MenuBouquet(new Scanner(System.in)).start();
     }
 }
