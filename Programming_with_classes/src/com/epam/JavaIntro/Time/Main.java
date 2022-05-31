102a41988fda922f06ebcf40087bbcda46a241c2
KorolevaOlga <84726342+KorolevaOlga@users.noreply.github.com>
2022-05-31 18:04:06 +0300

    Update Main.java

--- Programming_with_classes/src/com/epam/JavaIntro/Time/Main.java
+++ Programming_with_classes/src/com/epam/JavaIntro/Time/Main.java
@@ -8,7 +8,7 @@ package com.epam.JavaIntro.Time;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	
         Time watch = new Time(15, 23, 18);
         Time www = new Time(45, 88 , 66);
 
