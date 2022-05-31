49775aa8b2510b410acd9f99c930aa141d154d6c
KorolevaOlga <84726342+KorolevaOlga@users.noreply.github.com>
2022-05-31 18:09:33 +0300

    Update Main.java

--- Programming_with_classes/src/com/epam/JavaIntro/Triangle/Main.java
+++ Programming_with_classes/src/com/epam/JavaIntro/Triangle/Main.java
@@ -1,5 +1,7 @@
 package com.epam.JavaIntro.Triangle;
 
+// Описать класс, представляющий треугольник. Предусмотреть методы для создания объектов, вычисления площади, периметра и точки пересечения медиан.
+
 public class Main {
 
     public static void main(String[] args) {
