aafc52d537d38c0d0aa60b08f0cac3714027f020
KorolevaOlga <84726342+KorolevaOlga@users.noreply.github.com>
2022-05-31 17:19:49 +0300

    Create Main.java

---
+++ Basics_of_OOP/src/com/epam/JavaIntro/Flowers/main/Main.java
@@ -0,0 +1,10 @@
+package com.epam.JavaIntro.Flowers.main;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+      new MenuBouquet(new Scanner(System.in)).start();
+    }
+}
